0cf8ccfa9326e9df88b0190e8faab4604e0447bf
veenorth <ham0nug@gmail.com>
2024-11-10 18:42:36 +0900

    ac

---
+++ app/src/main/java/com/example/lessonnineapp/Splash_screen.kt
@@ -0,0 +1,26 @@
+package com.example.lessonnineapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        },2000)
+    }
+}
